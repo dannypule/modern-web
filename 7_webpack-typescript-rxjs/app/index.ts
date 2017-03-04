2639aeb9cbe24d839658b9621ba4a48fd857ec75
dannypule <kojikusan@gmail.com>
2017-03-04 12:02:37 +0000

    dom drawing with rxjs

--- 7_webpack-typescript-rxjs/app/index.ts
+++ 7_webpack-typescript-rxjs/app/index.ts
@@ -1,48 +1,27 @@
-import { ten } from './module';
 import * as Rx from 'rxjs/Rx';
 
-class Animal {
-    private _weight: number;
-
-    constructor(
-        private name: string,
-        private owner: string
-    ) {
-
-    }
-
-    getInfo() {
-        document.write(`
-            ${this.name} belongs to ${this.owner}. <br/> 
-        `);
-    }
-
-    get weight(): number {
-        return this._weight;
-    }
-
-    set weight(weight: number) {
-        this._weight = weight;
-    }
-}
-
-let dog = new Animal('Pogo', 'Jim');
-dog.getInfo();
-
-dog.weight = ten;
-let weight = dog.weight;
-
-document.write(`
-    Weight is ${weight}. <br/>
-`);
-
-let thing: any = document.getElementById('thing');
-let mousemove = Rx.Observable.fromEvent(document.body, 'mousemove');
-mousemove.subscribe((e: any)=>{
+let canDraw: boolean = false;
+let draw: any = (x: number, y: number):void => {
     let el = document.createElement('span');
-    el.classList.add('thing');
-    el.style.top = e.y + 'px';
-    el.style.left = e.x + 'px';
-    console.log(e.y);
-    document.body.appendChild(el);
+        el.classList.add('dot');
+        el.style.top = y + 'px';
+        el.style.left = x + 'px';
+        document.body.appendChild(el);
+};
+let thing: any = document.getElementById('dot');
+let mousemove = Rx.Observable.fromEvent(document.body, 'mousemove');
+let mousedown = Rx.Observable.fromEvent(document.body, 'mousedown');
+let mouseup = Rx.Observable.fromEvent(document.body, 'mouseup');
+mousemove.subscribe((e: any) => {
+    if (canDraw) {
+        draw(e.x, e.y);
+    }
 });
+mousedown.subscribe((e: any) => {
+    canDraw = true;
+    draw(e.x, e.y);
+});
+mouseup.subscribe((e: any) => {
+    canDraw = false;
+});
+
