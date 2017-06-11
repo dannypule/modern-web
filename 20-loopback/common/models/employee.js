5682bd05e672b8699a659d6e81b3aab60006588f
Danny Pule <danny.p@ri-team.com>
2017-06-11 22:23:39 +0100

    moar loopback

--- 20-loopback/common/models/employee.js
+++ 20-loopback/common/models/employee.js
@@ -1,5 +1,45 @@
 'use strict';
 
-module.exports = function(Employee) {
+module.exports = function (Employee) {
+    Employee.getAssetAmountForEmployee = function (id, cb) {
+        var filter = {
+            include: {
+                relation: 'assets',
+                scope: {
+                    fields: ['purchasePrice']
+                }
+            }
+        }
 
+        Employee.findById(id, filter, function (err, empl) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            var emplObj = empl.toJSON();
+            var totalAmount = 0;
+            emplObj.assets.forEach(asset => {
+                totalAmount += asset.purchasePrice;
+            });
+            cb(null, totalAmount);
+        });
+    };
+
+    Employee.remoteMethod('getAssetAmountForEmployee', {
+        description: 'Returns asset amount for employee',
+        accepts: {
+            arg: 'id',
+            type: 'string',
+            required: true
+        },
+        http: {
+            path: '/:id/getAssetAmountForEmployee',
+            verb: 'get'
+        },
+        returns: {
+            arg: 'amount',
+            type: 'number'
+        }
+    })
 };
