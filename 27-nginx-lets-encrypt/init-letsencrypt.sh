4d8d62cf537d99eadfa416e1183fa83cee863641
Danny Pule <kojikusan@gmail.com>
2020-05-20 15:30:40 +0100

    update

--- 27-nginx-lets-encrypt/init-letsencrypt.sh
+++ 27-nginx-lets-encrypt/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ssl.dannypule.com www.ssl.dannypule.com)
+domains=(testing.dannypule.com www.testing.dannypule.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="kojikusan@gmail.com" # Adding a valid address is strongly recommended
